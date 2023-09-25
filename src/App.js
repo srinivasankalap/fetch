5be7df2641700f64558b84957789fcd78b2c27c3
srinivasankalap <cnu.ankalap@gmail.com>
2023-09-25 14:07:57 +0530

    Error Handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -6,20 +6,36 @@ import './App.css';
 function App() {
   const [movies, setMovies]= useState([]);
   const [isLoading, setIsLoading]=useState(false);
+  const [error, setError]=useState(null);
 
-  async function fetchHandler(){
+  const fetchHandler=useCallback(async()=>{
     setIsLoading(true);
-    const response= await fetch('https://swapi.dev/api/films/');
-    const data =await response.json();
-    const transformed=data.results.map((movie)=>{
-      return {id: movie.episode_id,
-      title: movie.title,
-      openingText: movie.opening_crawl,
-      releaseDate: movie.release_date}
-    })
-    setMovies(transformed);
+    setError(null);
+    try{
+      const response= await fetch('https://swapi.dev/api/films/');
+      if (!response.ok){
+        throw new Error('Something went wrong.....Retrying');
+      }
+      const data =await response.json();
+
+      const transformed=data.results.map((movie)=>{
+        return {id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date}
+      })
+      setMovies(transformed);
+      setIsLoading(false);
+    }
+    catch(error){
+      setError(error.message);
+    }
     setIsLoading(false);
-  }
+  },[]);
+  useEffect(()=>{
+    fetchHandler();
+  },[fetchHandler]);
+
   return (
     <React.Fragment>
       <section>
@@ -27,6 +43,7 @@ function App() {
       </section>
       <section>
         {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
